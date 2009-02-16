bf3125409520ae6b98315d3a301a16894e1701c5
vasiltsov <vasiltsov@d67282b6-fa03-11dd-a7be-4b3ef3b0700c>
2009-02-16 14:36:49 +0000

    Made example easier
    git-svn-id: https://tar-cs.googlecode.com/svn/trunk@27 d67282b6-fa03-11dd-a7be-4b3ef3b0700c

--- tar/Program.cs
+++ tar/Program.cs
@@ -17,7 +17,6 @@ namespace tar
                 dataSize -= 512;
                 yield return buffer;
             }
-
         }
 
         private static void Main(string[] args)
@@ -28,44 +27,9 @@ namespace tar
                 return;
             }
 
-            if(args.Length == 2 && args[0] == "--test-big")
+            using(var archUsTar = File.Create(args[0]))
             {
-                using(var file = File.Create(args[1]))
-                using(var tar = new UsTarWriter(file))
-                {
-                    IEnumerator<byte[]> i = NextDataBlock(9255295864);
-
-                    tar.Write("bigFile.bin", 9255295864, "v", "v", 0777, DateTime.Now, delegate(IDataWriter writer)
-                    {
-//                        i.MoveNext();
-//                        byte[] current = i.Current;
-//                        writer.Write(current, current.Length);
-                    });
-                }
-                
-                return;
-            }
-
-            using (FileStream archiveFile = File.Create(args[0]))
-            {
-                using (TarWriter tar = new TarWriter(archiveFile))
-                {
-                    for (int i = 1; i < args.Length; ++i)
-                    {
-                        tar.Write(args[i]);
-                    }
-                    MemoryStream stream = new MemoryStream();
-                    StreamWriter writer = new StreamWriter(stream);
-                    writer.WriteLine("This archive created with tar-cs.");
-                    writer.Flush();
-                    stream.Position = 0;
-                    tar.Write(stream, stream.Length, "README.TXT");
-                }
-            }
-
-            using(var archUsTar = File.Create(args[0]+".ustar.tar"))
-            {
-                using(var tar = new UsTarWriter(archUsTar))
+                using(var tar = new TarWriter(archUsTar))
                 {
                     for (int i = 1; i < args.Length; ++i)
                     {
